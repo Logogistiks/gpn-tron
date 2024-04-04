9cb7d69f381e9904afc2a7bdf79dbb8b2b444b1a
Jan <jan@seifert-online.de>
2024-04-04 21:58:32 +0200

    fixed some shit, bot is now as good as serverside bots

--- bot/logic.py
+++ bot/logic.py
@@ -1,7 +1,9 @@
 """Handles the game logic."""
 
+from random import choice
+
 # local
-from utils import randMove
+from utils import DIRECTIONS, randMove
 
 class Player:
     def __init__(self, pID: str, name: str, posX: str=None, posY: str=None) -> None:
@@ -27,7 +29,7 @@ class Player:
                 self.dir = "up"
         self.pos.append((newPosX, newPosY))
 
-    def getPos(self) -> tuple[int, int]:
+    def getPos(self) -> tuple[str, str]:
         """Returns the player's current position."""
         return self.pos[-1]
 
@@ -40,7 +42,7 @@ class GameHandler:
         """Creates a game object."""
         self.sizeX = int(sizeX)
         self.sizeY = int(sizeY)
-        self.map = [[" "]*int(sizeX)]*int(sizeY)
+        self.grid = [[" " for _ in range(int(sizeX))] for _ in range(int(sizeY))]
         self.players: dict[str, Player] = {}
         self.myID = pID
         self.wins = 0
@@ -50,16 +52,22 @@ class GameHandler:
         """Adds a player to the game."""
         if not None in (posX, posY):
             self.players[pID] = Player(pID, name, posX, posY)
+            self.grid[int(posY)][int(posX)] = pID
         else:
             self.players[pID] = Player(pID, name)
 
     def remPlayer(self, pID: str) -> None:
         """Removes a player from the game."""
         self.players.pop(pID, None)
+        for y in range(self.sizeY):
+            for x in range(self.sizeX):
+                if self.grid[y][x] == pID:
+                    self.grid[y][x] = " "
 
     def updatePlayerPos(self, pID: str, posX: str, posY: str) -> None:
         """Updates a player's position."""
         self.players[pID].updatePos(posX, posY, self.sizeX, self.sizeY)
+        self.grid[int(posY)][int(posX)] = pID
 
     def getMe(self) -> Player:
         """Returns the player object of the bot."""
@@ -69,14 +77,36 @@ class GameHandler:
         """Returns all the other players except the bot."""
         return {k: v for k, v in self.players.items() if k != self.myID}
 
+    def getNewPos(self, posX: str, posY: str, dir: str) -> tuple[str, str]:
+        """Returns the new position after moving in the given direction."""
+        if dir == "up":
+            return posX, str((int(posY) - 1) % self.sizeY)
+        if dir == "down":
+            return posX, str((int(posY) + 1) % self.sizeY)
+        if dir == "left":
+            return str((int(posX) - 1) % self.sizeX), posY
+        if dir == "right":
+            return str((int(posX) + 1) % self.sizeX), posY
+
     def nextMove(self, pID: str=None) -> str: # has ability to simulate moves of other players
-        """Returns the next move for a player"""
+        """Moves the player and returns the move."""
         if pID is None:
             pID = self.myID
         newMove = self.calcMove(pID)
         self.players[pID].dir = newMove
         return newMove
 
+    '''
     def calcMove(self, pID: str) -> str:
-        """Calculates the next move for the bot."""
-        return randMove(self.players[pID].dir) #todo: implement better logic
+        """Calculates the next move for a player."""
+        return randMove(self.players[pID].dir)
+    '''
+
+    def calcMove(self, pID: str) -> str:
+        """Calculates the next move for a player."""
+        possibleMoves = []
+        for dir in DIRECTIONS:
+            newPos = self.getNewPos(*self.players[pID].getPos(), dir)
+            if self.grid[int(newPos[1])][int(newPos[0])] == " ":
+                possibleMoves.append(dir)
+        return choice(possibleMoves) if possibleMoves else "up" # theres nothing we can do but surrender
