c69eb6945b10fbdf0b65eae479219ef64337c346
Jan <jan@seifert-online.de>
2024-04-05 20:30:47 +0200

    Update setup.py

--- bot/setup.py
+++ bot/setup.py
@@ -1,9 +1,12 @@
 import os
 
-if not os.path.exists("auth.csv"):
-    with open("auth.csv", "w") as f:
+# local
+from utils import file
+
+if not os.path.exists(file("auth.csv")):
+    with open(file("auth.csv"), "w") as f:
         f.write("user;pass\n")
 
-if not os.path.exists("stats.txt"):
-    with open("stats.csv", "w") as f:
+if not os.path.exists(file("stats.csv")):
+    with open(file("stats.csv"), "w") as f:
         f.write("wins;losses\n0;0")
