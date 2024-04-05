4d3fd0d222bbcb2fb2ddf704724dac7dfe9bf181
Jan <jan@seifert-online.de>
2024-04-05 22:33:50 +0200

    added splash file creation

--- bot/setup.py
+++ bot/setup.py
@@ -6,3 +6,7 @@ from utils import file
 if not os.path.exists(file("auth.csv")):
     with open(file("auth.csv"), "w") as f:
         f.write("user;pass\n")
+
+if not os.path.exists(file("splashes.txt")):
+    with open(file("splashes.txt"), "x") as f:
+        pass
