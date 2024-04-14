4274b32064b3fb94b06337f499b533ceb5fd605c
Jan <jan@seifert-online.de>
2024-04-14 19:46:14 +0200

    Update setup.py
    added default user+pass uuid

--- bot/setup.py
+++ bot/setup.py
@@ -1,11 +1,12 @@
 import os
+from uuid import uuid4
 
 # local
 from utils import file
 
 if not os.path.exists(file("auth.csv")):
     with open(file("auth.csv"), "w") as f:
-        f.write("user;pass\n")
+        f.write(f"user;pass\n{uuid4().hex[:10]};{uuid4().hex[:10]}")
 
 if not os.path.exists(file("splashes.txt")):
     with open(file("splashes.txt"), "x") as f:
